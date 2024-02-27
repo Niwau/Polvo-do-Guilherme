272cb7c15d3af06e38c0a162688fc1fdead06e46
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-02-27 11:04:13 -0300

    chore: change structure to typescript template

--- src/commands/utility/ping.js
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-export const pingCommand = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
-  execute: async (interaction) => {
-    await interaction.reply("Pong!");
-  },
-};
